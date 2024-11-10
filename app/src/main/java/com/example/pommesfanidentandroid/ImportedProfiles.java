f39eff94f72267331444f409309d436115276fa4
Johannes Wirbser <johannes.wirbser@gmx.de>
2024-11-10 20:54:02 +0100

    can open file(just problems with file reference)

--- app/src/main/java/com/example/pommesfanidentandroid/ImportedProfiles.java
+++ app/src/main/java/com/example/pommesfanidentandroid/ImportedProfiles.java
@@ -2,13 +2,24 @@ package com.example.pommesfanidentandroid;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
+import android.os.FileUtils;
+import android.util.Log;
+import android.widget.Toast;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.example.controller.Controller;
+
+import java.io.File;
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
 
 public class ImportedProfiles extends Activity {
+    private static final int PICKFILE_RESULT_CODE = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +32,37 @@ public class ImportedProfiles extends Activity {
         findViewById(R.id.addButton).setOnClickListener(v -> openFile());
     }
 
+    private static final int FILE_SELECT_CODE = 0;
+
     private void openFile() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
-        intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, "");
-        startActivityForResult(intent, 1);
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    Uri uri = data.getData();
+                    File f = new File(uri.getPath());
+                    try {
+                        Controller.controller.importPublicProfile(f);
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
